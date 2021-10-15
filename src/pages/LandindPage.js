ab206aae41bb408efe4718af8e7e7c007ce45e82
OWNER <obogomaty@gmail.com>
2021-10-15 09:18:38 -0700

    adjusted some styling

--- src/pages/LandindPage.js
+++ src/pages/LandindPage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import './landingPage.scss'
-import event from '../images/event.png'
+import './landingPage.scss';
+import event from '../images/event.png';
 
 function LandingPage() {
      return (
           <div className='background-image'>
-               <img src={event} width="300" height="300" z-index="2" position="relative" filter="brightness(80%)" />
+               <img src={event}/>
                <p className='firstParagraph'>Get Closer to your <span>social</span> Meetup</p>
                <p className='secondParagraph'>Work hard play harder,exclusive VIP access to the best</p>
                <p className='thirdParagraph'>events,parties and everything <span>FUN!!!</span></p>
