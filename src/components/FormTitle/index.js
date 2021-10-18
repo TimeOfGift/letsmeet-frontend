c3953d2face906320a74e29e660d0e4fbacea575
jstar <drealjstar@yahoo.com>
2021-10-17 23:33:22 -1200

    remove unused css properties

--- src/components/FormTitle/index.js
+++ src/components/FormTitle/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./formTitle.scss";
 
 
-const FormTitle = ({ text }) => {
+const FormTitle = ({ text, textColor, rectWidth }) => {
     return (
         <div className="form_title">
-            <h1 className="form_title_update">{text}</h1>
-            <div className="update_rectangle"></div>
+            <h1 className="form_title_update" style={{color : textColor}}>{text}</h1>
+            <div className="update_rectangle" style={{width : rectWidth}}></div>
         </div>
     );
 }
