06d19490d6091943a57297f293dfb70f386d0138
hammed noibi <hnobi08@yahoo.com>
2021-10-23 14:07:46 +0100

    fix general design issue

--- src/components/EventTitle/index.jsx
+++ src/components/EventTitle/index.jsx
@@ -5,17 +5,14 @@ const EventTitle = (props) => {
 
 		const { topic, subTopic, street, state, date, time } = props;
 		return (
-			<div className="eventdetail">
-
 				<div className="eventtitles">
-
 					<div className="topics">
-						<h1>{topic}</h1>
+						<p className="to">{topic}</p>
 						<p>{subTopic}</p>
 						<button className="btnrsvp">RSVP</button>
 					</div>
 					<div className="location">
-						<h6>Location</h6>
+						<p className="loc">Location</p>
 						<p className="street">{street}</p>
 						<p className="state">{state}</p>
 					</div>
@@ -25,8 +22,6 @@ const EventTitle = (props) => {
 						<p className="time">{time}</p>
 					</div>
 				</div>
-
-			</div>
 		);
 	}
 
