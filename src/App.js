d65e26ac3e918b2837a0e9c0138a52ddd510d4be
Mors Etim <maur@gmail.com>
2021-10-24 11:07:03 +0100

    yarn lock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import EventDetails from './components/EventDetails/EventDetails';
-import Events from './components/EventDetails/EventObj'
+import React from 'react';
 import './App.scss';
-=======
 import "./App.scss";
 import Routes from './Routes'
->>>>>>> e81d0bd29b025963915edfb3857e08ddb93be440
 
 
 function App() {
