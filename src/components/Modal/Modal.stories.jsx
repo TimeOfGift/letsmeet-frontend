bae24a15aceb8e107c5ea1840415be8e755ca98c
obogomaty <obogomaty@gmail.com>
2021-10-24 10:38:49 -0700

    modalpopup

---
+++ src/components/Modal/Modal.stories.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Modal from './index';
+
+export default {
+    title: 'Event Meet/ModalPopUp',
+    component: Modal,
+    args: {
+
+    }
+}
+
+export const All = (args) => <Modal {...args} />
