94cc079c33aab3238bd818e22e09ac165201cd7f
Miracle Nwaubani <Liegeubani@gmail.com>
2021-10-29 17:58:09 +0100

    Notification dropdown component. (#17)
    * Ft Notification dropdown component.
    
    * implemented feed back on notification dropdown
    
    * implemented feed back on notification dropdown
    
    * add padding
    
    Co-authored-by: hammed noibi <hnobi08@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.scss';
 import "./App.scss";
-import Routes from './Routes'
-
+import Routes from "./Routes";
 
 function App() {
-
   return (
     <div className="App">
       <Routes />
