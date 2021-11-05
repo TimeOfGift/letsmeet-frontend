1f37d0f8b1319434da2d39bd122dfed42cd5fdf1
Obogo Mathias <obogomaty@gmail.com>
2021-11-05 09:00:13 +0100

    nade changes

--- src/components/FormTitle/index.js
+++ src/components/FormTitle/index.js
@@ -6,7 +6,7 @@ const FormTitle = ({ className,text, textColor, rectWidth, rectColor }) => {
     return (
         <div className="form_title">
             <h1 className={`form_title_update ${className}`} style={{color : textColor}}>{text}</h1>
-            <div className="update_rectangle" style={{width : rectWidth, background: rectColor}}></div>
+            <div className="update_rectangle" style={{  width : rectWidth, background: rectColor}}></div>
         </div>
     );
 }
