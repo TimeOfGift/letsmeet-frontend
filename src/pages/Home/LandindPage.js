87511383d182ab826c56aa5d1ac49b592258570f
drealjstar <drealjstar@yahoo.com>
2021-11-06 12:23:24 +0100

    linking home to signup

--- src/pages/Home/LandindPage.js
+++ src/pages/Home/LandindPage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './landingPage.scss';
 import event from '../../images/event.png';
-import Button from '../../components/Button'
-
+import Button from '../../components/Button';
+import {Link} from "react-router-dom";
 
 function LandingPage() {
      return (
@@ -12,8 +12,9 @@ function LandingPage() {
                <p className='secondParagraph'>Work hard play harder,exclusive VIP access to the best</p>
                <p className='secondParagraph'>events,parties and everything <span>FUN!!!</span></p>
                {/* <button className='landingPagebutton'>Join the creed now! </button> */}
+               <Link to= "/signup" className="sig">
                <Button text='Join the creed now!' className='landingPagebutton'/>
-
+               </Link>
           </div>
      );
 }
