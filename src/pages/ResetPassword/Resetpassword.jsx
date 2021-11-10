5a4f4c98adf2bf7d4ad4db77ee6988dce4ec6297
Obogo Mathias <obogomaty@gmail.com>
2021-11-10 11:55:36 +0100

    rebasing

--- src/pages/ResetPassword/Resetpassword.jsx
+++ src/pages/ResetPassword/Resetpassword.jsx
@@ -1,42 +1,40 @@
 import React, { useState } from 'react'
 import './Resetpassword.scss'
 import Button from "./../../components/Button"
+import FormTitle from './../../components/FormTitle'
 
 
 const Resetpassword = () => {
 
-    const [password,setPassword] = useState("")
+    const [password, setPassword] = useState("")
 
-    const handleChange = (e) =>{
-      setPassword([e.target.password],e.target.value)
+    const handleChange = (e) => {
+         setPassword( e.target.value)
     }
 
     const handleSubmit = (e) => {
-       e.preventDefault();
-        
+        e.preventDefault();
+
     }
 
     return (
-
-        <div className="containerhouse">
-            <div className="maincontainer">
-                <div className='firstparagraph'>
-                    <p className="resetpassword">Passwords Reset </p>
-                </div>
-                <hr className="figma"></hr>
-                <div className="secondparagraph">
+        <div handleClick={handleSubmit}>
+            <div className="containerhouse">
+                <div className="maincontainer">
+                    <FormTitle className="resetpassword" text="Reset Password" rectColor="none" />
+                    <hr className="figma"></hr>
                     <p className="passwordentry" > Enter new password</p>
-                </div>
-                <div>
-                    <input type="text" placeholder="Password" className="passwordinput" value={password}
-                        onChange={handleChange} />
-                </div>
-                <div className="buttondiv">
+                    <div>
+                        <input type="password" placeholder="Password" className="passwordinput" value={password}
+                            onChange={handleChange} />
+                    </div>
                     <Button className="corebutton" text="Reset Password" handleClick={handleSubmit}></Button>
-                </div>
 
                 </div>
+            </div>
         </div>
+
+
 
 
     )
