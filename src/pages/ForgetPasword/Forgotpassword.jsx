6007939e26fe89f5bae84dd95a8777eb45f54c3f
Obogo Mathias <obogomaty@gmail.com>
2021-11-10 11:44:48 +0100

    endment

---
+++ src/pages/ForgetPasword/Forgotpassword.jsx
@@ -0,0 +1,47 @@
+import react, { useState } from 'react'
+import './Forgotpassword.scss'
+
+const Forgotpassword = () => {
+
+    const [email, setEmail] = useState("")
+
+    const onChange = (e) =>{
+         setEmail([e.target.email],e.target.value)
+     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('email')
+    }
+
+    return (
+
+        <div className="containerhouse">
+            <div className="maincontainer">
+                <div className='firstparagraph'>
+                    <p className="forgotpassword">Forgot Passwords</p>
+                </div>
+                <hr className="figma"></hr>
+                <div className="secondparagraph">
+                    <p className="emailentry" >Enter your email address</p>
+                </div>
+                <div className="emailadress">
+                    <input type="text" placeholder="email address" className="emailinput" value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="buttondiv">
+                    <button onSubmit={handleSubmit} className="corebutton">submit</button>
+                </div>
+
+
+            </div>
+        </div>
+
+
+    )
+
+}
+
+
+export default Forgotpassword;
+
