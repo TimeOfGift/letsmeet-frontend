514d1cc97ed6bc1f0f02b8a9a97cca730f0544be
Obogo Mathias <obogomaty@gmail.com>
2021-11-12 00:07:54 +0100

    codeadjustment

---
+++ src/pages/resetpassword/resetpassword.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './Resetpassword.scss'
+import Button from "./../../components/Button"
+import FormTitle from './../../components/FormTitle'
+
+
+const Resetpassword = () => {
+
+    const [password, setPassword] = useState("")
+
+    const handleChange = (e) => {
+         setPassword( e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    return (
+        <div handleClick={handleSubmit}>
+            <div className="containerhouse">
+                <div className="maincontainer">
+                    <FormTitle className="resetpassword" text="Reset Password" rectColor=" #FFA500CC" />
+                    <p className="passwordentry" > Enter new password</p>
+                    <div>
+                        <input type="password" placeholder="Password" className="passwordinput" value={password}
+                            onChange={handleChange} />
+                    </div>
+                    <Button className="corebutton" text="Reset Password" handleClick={handleSubmit}></Button>
+
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+export default Resetpassword;
