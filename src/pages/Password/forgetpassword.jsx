fc1f2d92d663e5ee9b096a78233f477f98dbd9ef
Obogo Mathias <obogomaty@gmail.com>
2021-11-13 23:54:44 +0100

    code adjustment

--- src/pages/Password/forgetpassword.jsx
+++ src/pages/Password/forgetpassword.jsx
@@ -9,35 +9,32 @@ const Forgetpassword = () => {
 
     const [email, setEmail] = useState("")
 
-    const handleChange = (e) =>{
-         setEmail(e.target.value)
-     }
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+    }
 
-     const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log('email')
-     }
+    }
 
     return (
-           <form >
-        <div className="containerhouse">
-            <div className="maincontainer">
-                <div className='firstparagraph'>
-                    <FormTitle  text="Forgot Password" rectColor="#FFA500CC" textColor='black'/>
-                </div>
-                
-                <div className="secondparagraph">
-                    <p className="entry" >Enter your email address</p>
-                </div>
-                <div className="emailadress">
-                     <input type="text" placeholder="Email address" value={email}
-                        onChange={handleChange} /> 
-                </div>
-                <div className="buttondiv">
-                    <Button className="corebutton" text="Submit" radius='9px'/>            
+        <form >
+            <div className="containerhouse">
+                <div className="maincontainer">
+                    <FormTitle text="Forgot Password" rectColor="#FFA500CC" textColor='black' />
+                    <div className="secondparagraph">
+                        <p className="entry" >Enter your email address</p>
+                    </div>
+                    <div className="emailadress">
+                        <input type="text" placeholder="Email address" value={email}
+                            onChange={handleChange} />
+                    </div>
+                    <div className="buttondiv">
+                        <Button className="corebutton" text="Submit" radius='9px' />
+                    </div>
                 </div>
             </div>
-        </div>
 
         </form>
     )
