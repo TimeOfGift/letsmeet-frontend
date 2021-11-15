31a4c9aa5bba7636127d9edc9780c349a05b5ffd
hammed noibi <hnobi08@yahoo.com>
2021-11-15 15:18:14 +0100

    cleanup

--- src/pages/Password/resetpassword.jsx
+++ src/pages/Password/resetpassword.jsx
@@ -15,22 +15,34 @@ const Resetpassword = () => {
     
 
     return (
-        <div>
-            <div className="containerhouse">
-                <div className="maincontainer">
-                    <FormTitle className="resetpassword" text="Reset Password" rectColor=" #FFA500CC" />
-                    <p className="entry" > Enter new password</p>
-                    <div>
-                        <input type="password" placeholder="Password"  value={password}
-                            onChange={handleChange} />
-                    </div>
-                    <Button textColor="black" radius='9px' className="corebutton" text="Reset Password"></Button>
-
-                </div>
+      <div>
+        <div className="containerhouse">
+          <div className="maincontainer">
+            <FormTitle
+              textColor="black"
+               className="resetpassword"
+              text="Reset Password"
+              rectColor=" #FFA500CC"
+            />
+            <p className="entry"> Enter new password</p>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChange}
+              />
             </div>
+            <Button
+              textColor="black"
+              radius="9px"
+              className="corebutton"
+              text="Reset Password"
+            ></Button>
+          </div>
         </div>
-
-    )
+      </div>
+    );
 
 }
 export default Resetpassword;
