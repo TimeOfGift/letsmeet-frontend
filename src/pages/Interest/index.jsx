67c66e8d6a28c9977a536f26f457bf22ef82fce1
drealjstar <drealjstar@yahoo.com>
2022-09-05 14:53:50 +0100

    add i nterest

---
+++ src/pages/Interest/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './interestlist.scss';
+
+
+
+function  InterestList() {
+     return (
+          <div className='interest_list'>
+               <div className="interest">
+
+               </div>
+        
+
+
+
+
+          </div>
+     );
+}
+
+export default InterestList;
