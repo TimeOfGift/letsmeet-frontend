dcb4fcd7dccd36fa65ce9c1266efab194ef25983
drealjstar <drealjstar@yahoo.com>
2022-09-05 16:18:30 +0100

    add new code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import "./App.scss";
 import Routes from "./Routes";
+import InterestList from './pages/Interest';
 
 function App() {
   return (
     <div className="App">
+      <InterestList />
       <Routes />
     </div>
   );
