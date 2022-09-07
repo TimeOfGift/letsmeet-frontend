5be9888c2327b42c1042c472ca6683538a5fe88f
drealjstar <drealjstar@yahoo.com>
2022-09-07 13:06:37 +0100

    update state

--- src/pages/Interest/index.jsx
+++ src/pages/Interest/index.jsx
@@ -1,38 +1,31 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchInterests } from './../../redux/actions/interests'
 import "./interestlist.scss";
 import InterestButton from "../../components/InterestButton/interestButton";
-import axios from "axios";
 
-function  InterestList() {
+function InterestList() {
+     const dispatch = useDispatch();
+     const { interests } = useSelector(state => state.interestReducer);
 
+     console.log(interests, "=============================")
      const [item, setItem] = useState([]);
 
 
      useEffect(() => {
-          axios.get("https://event-meet.herokuapp.com/api/v1/user/interest")
-          .then(result =>{
-               console.log(result, 'lllllllll');
-          })
-         .catch(error =>{
+          dispatch(fetchInterests())
 
-          console.log(error);
-         })
-
-     });
+     }, []);
 
 
      return (
           <div className='interest_list'>
-          <div className="item">
-                
-             <InterestButton />
-
-
-
-
-
-               </div>
-           </div>
+               {interests?.map(interest => (
+                    <div className="item">
+                         <InterestButton name={interest.name} />
+                    </div>
+               ))}
+          </div>
      );
 }
 
